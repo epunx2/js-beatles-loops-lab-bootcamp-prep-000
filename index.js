c38632196431b9bd2dba8f9ffe70bb6f193f33ef
Eric Chrobak <eric@ericchrobak.com>
2018-01-22 02:50:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var theBeatlesPlay = function(musiciansArr, instrumentsArr) {
   
 }
+
+var johnLennonFacts = function(jlFacts) {
+  while(var i = 0; i < jlfacts.length()){
+    jlfacts[i] = jlfacts[i] + "!";
+    i++;
+  }
+  return jlfacts;
+}
+
+var iLoveTheBeatles = function(num) {
+  
+}
